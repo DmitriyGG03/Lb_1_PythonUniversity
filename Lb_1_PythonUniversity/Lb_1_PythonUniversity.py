09f3cd97e3850c6545e6b88aae1ffaba10de56fd
Dmitriy Gonchar <dmytro.honchar@nure.ua>
2023-03-23 17:25:28 +0200

    Release

--- Lb_1_PythonUniversity/Lb_1_PythonUniversity.py
+++ Lb_1_PythonUniversity/Lb_1_PythonUniversity.py
@@ -1,1 +1,29 @@
+import sys
+import msvcrt
+import os
 
+
+inputStrings = ""
+data = []
+
+while True:
+    os.system('cls')
+    print("Enter strings:")
+    while True:
+        inputStrings = input()
+    
+        if inputStrings == "exit":
+            sys.exit()
+        elif inputStrings == "":
+            break
+        else:
+            data.append(inputStrings)
+
+    if len(data) == 0:
+        print("No data")
+    
+    print("Output result:")
+    for str in data:
+        print(str[0] + str[len(str) - 1])   
+    print("\nPress any key to input again")
+    msvcrt.getch()
